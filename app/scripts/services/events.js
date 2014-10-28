904f8a56e254159fea939bf7dcdb03b268a8a987
Vlad <vlad.donets@somuchmore.org>
2014-10-28 22:35:01 +0300

    add comments

--- app/scripts/services/events.js
+++ app/scripts/services/events.js
@@ -8,10 +8,10 @@
  * Factory in the boltApp.
  */
 angular.module('boltApp.services.events', [])
-    .factory('Events', ['$resource', function($resource) {
-        return $resource('http://stage-smm-api.herokuapp.com/api/occurrence/:eventId', {eventId: '@id'}, {
+    .factory('Events', ['$resource', '$window', function($resource, $window) {
+        return $resource('//' + $window.smm_config.rest_url + '/api/occurrence/:eventId', {eventId: '@id'}, {
             'query': {method: 'GET', isArray: false, cache: true},
             'get': {method: 'GET', cache: true},
-            'getOrder': {method: 'GET', cache: false, url: 'http://stage-smm-api.herokuapp.com/api/eventbrite/:eventId/order'}
+            'getOrder': {method: 'GET', cache: false, url: '//' + $window.smm_config.rest_url + '/api/eventbrite/:eventId/order'}
         });
     }]);
--- app/scripts/services/suppliers.js
+++ app/scripts/services/suppliers.js
@@ -8,10 +8,9 @@
  * Service in the boltApp.
  */
 angular.module('boltApp.services.suppliers', [])
-    .factory('Suppliers', ['$resource', function($resource) {
-        return $resource('//smm-dev.herokuapp.com/api/rest/supplier/:supplierId', {eventId: '@id'}, {
-            'query': {method: 'GET', url: '//smm-dev.herokuapp.com/api/rest/supplier/list', isArray: true, cache: true},
+    .factory('Suppliers', ['$resource', '$window', function($resource, $window) {
+        return $resource('//' + $window.smm_config.rest_url_old + '/api/rest/supplier/:supplierId', {eventId: '@id'}, {
+            'query': {method: 'GET', url: '//' + $window.smm_config.rest_url_old + '/api/rest/supplier/list', isArray: true, cache: true},
             'get': {method: 'GET', cache: true}
         });
     }]);
-
