ab0714285aaccf36030b8a8756b0cf012d638176
Дмитрий Власенко <dmitrijvlasenko@MacBook-Air-Dmitrij.local>
2015-03-25 00:12:48 +0200

    Uncommited tests

--- test/acceptence/tests.js
+++ test/acceptence/tests.js
@@ -1,10 +1,10 @@
 'use strict';
 
-// require('./checkPages.js');
-// require('./homepage.js');
-// require('./linkedPages.js');
-// require('./resetPassword.js');
-// require('./signup.js');
-// require('./photoUpload.js');
-// require('./inviteRequest.js');
+require('./checkPages.js');
+require('./homepage.js');
+require('./linkedPages.js');
+require('./resetPassword.js');
+require('./signup.js');
+require('./photoUpload.js');
+require('./inviteRequest.js');
 require('./changeCity.js');
