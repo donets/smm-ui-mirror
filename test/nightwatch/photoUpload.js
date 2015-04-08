430457f9be626c92726e8f6e6cdc90b8c7f1598c
macabre133 <xenixlx@gmail.com>
2015-04-08 17:29:54 +0300

    fixed tests rewrote some of them and encountered unknown error in phantomJS in photoUpload.js For now tests are executed under firefox...

--- test/nightwatch/photoUpload.js
+++ test/nightwatch/photoUpload.js
@@ -1,68 +1,38 @@
-// /* global browser, element, by */
+// it is not working with phantomJS i don't know why but phantomJS just loops in itself without any errors
+module.exports = {
+  tags: ['login','image','upload'],
 
-// 'use strict';
+  'LogIN': function (browser) {
+    var params = browser.globals;
+    browser
+      .url(params.baseUrl)
+      .windowMaximize()
+      .waitForElementVisible('body', 1000)
+      .click('a.login-link.ng-scope > span.ng-scope')
+      .waitForElementVisible('input[name=email]', 1000)
+      .setValue('input[name=email]', params.signup.email)
+      .setValue('input[name=password]', params.signup.password)
+      .click('button[type=submit]')
+      .waitForElementVisible('h1 > span.ng-scope', 5000)
+      .assert.containsText('h1 > span.ng-scope', 'Finde einen Kurs')
+  },
 
-// describe('Given the Kurse Page, User', function() {
-    // beforeEach(function() {
-        // browser.get('/');
-    // });
-
-    // afterEach(function() {
-        // browser.driver.manage().deleteAllCookies();
-    // });
-
-    // var params = browser.params,
-        // myPassword = params.signup.password;
-    // var path = require('path');
-    // browser.driver.manage().window().maximize();
-
-    // it('should upload photo', function(done) {
-        // var login = element(by.css('#login'));
-        // login.click();
-
-        // element(by.css('form[name="loginForm"] input[name="email"]')).sendKeys(params.signup.email);
-        // element(by.css('form[name="loginForm"] input[name="password"]')).sendKeys(params.signup.password);
-        // element(by.css('form[name="loginForm"] button[type="submit"]')).click();
-
-        // expect(element(by.css('#account a')).getText()).toContain(params.signup.firstName + ' ' + params.signup.lastName);
-
-        // element(by.css('.success button')).click();
-        // element(by.css('#account')).click();
-
-        // var accountLink = element.all(by.css('.b-header-nav_login-widget ul li')).get(1);
-        // accountLink.click();
-
-        // browser.sleep(1000);
-
-        // var mainCart = element.all(by.css('.tab-nav li a')).get(0);
-        // mainCart.click();
-
-        // browser.sleep(1000);
-
-        // var showUploadForm = element.all(by.css('.photo .env-upload-box div')).get(0);
-        // showUploadForm.click();
-
-        // browser.sleep(1000);
-
-
-        // var chooseFileBtn = element.all(by.css('.button_upload input')).get(0),
-            // fileToUpload = '../../app/images/alexandra.jpg',
-            // absolutePath = path.resolve(__dirname, fileToUpload);
-        // browser.executeScript('$(\'.button_upload input\').css("visibility", "visible");');
-        // browser.sleep(1000);
-
-        // // browser.sleep(40000);
-        // chooseFileBtn.sendKeys(absolutePath);
-
-        // browser.sleep(3000);
-
-        // var sendPhotoBtn = element.all(by.css('.action .upload_button')).get(0);
-        // sendPhotoBtn.click();
-
-        // browser.sleep(5000);
-
-        // var photo = element.all(by.css('.photo img')).get(0);
-
-        // done();
-    // });
-// });
+  'Go to my account and upload photo': function (browser) {
+     var path = require('path');
+     var params = browser.globals;
+      fileToUpload = '../../app/images/alexandra.jpg',
+      absolutePath = path.resolve(__dirname, fileToUpload);
+    browser
+      .url(params.baseUrl + '/my/account/')
+      .waitForElementVisible('.profile-title > h2:nth-child(1) > span:nth-child(1)', 5000)
+      .assert.containsText('.profile-title > h2:nth-child(1) > span:nth-child(1)', 'Hallo,')
+      .click('.tab-nav > li:nth-child(1) > a:nth-child(1)')
+      .waitForElementVisible('div.env-upload-box > div',2000)
+      .click('div.env-upload-box > div')
+      .execute('$(\'.button_upload input\').css("visibility", "visible");')
+      .setValue('.button_upload input',absolutePath)
+      .click('.action .upload_button')
+      .waitForElementNotVisible('div.modal-dialog',5000)
+      .end();//add assertion of image for complete test
+  }
+};
--- test/nightwatch/resetPassword.js
+++ test/nightwatch/resetPassword.js
@@ -1,45 +1,36 @@
-// /* global browser, element, by */
+  module.exports = {
+  tags: ['resetPassword'],
 
-// 'use strict';
+  'reset password for test@somuchmore.org': function (browser) {
+	  var params = browser.globals;
+  browser
+    .url(params.baseUrl)
+    .waitForElementVisible('body', 1000)
+    .click('a.login-link.ng-scope > span.ng-scope')
+    .waitForElementVisible('.b-header-nav_login-widget',1000)
+    .waitForElementVisible('a.forgot-password:nth-child(1) > span:nth-child(1)',1000)
+    .click('a.forgot-password:nth-child(1) > span:nth-child(1)')
+    .waitForElementVisible('input[ng-model=emailForgot]',1000)
+    .setValue('input[ng-model=emailForgot]',params.signup.email)
+    .click('div.form-rel > button[ng-disabled=loadingForgot]')
+    .waitForElementVisible('div[ng-show=successSubscribe] > span',3000)
+    .assert.containsText('div[ng-show=successSubscribe] > span','E-Mail')
+    .end()
+  }
+}
+//        browser.get('http://shitmail.me/mail/inbox/somuchmore@shitmail.me');
+//        var btn = element(by.cssContainingText('a', 'Passwort reset request'));
+//        btn.click();
+//
+//        browser.driver.executeScript(function() {
+//            return document.querySelectorAll('.boxContent')[1].innerText.match('https.*')[0];
+//        }).then(function(result) {
+//            browser.get(result);
+//        });
+//
+//        var resetPage = element(by.css('.b-main-slogan'));
+//        expect(browser.getCurrentUrl()).toContain('/p/password');
+//
+//        element(by.css('form.b-card-subscribe input[type="password"]')).sendKeys(params.signup.password);
+//        element(by.css('form.b-card-subscribe button[type="submit"]')).click();
 
-// describe('Given password reset from, User', function() {
-
-    // beforeEach(function() {
-        // browser.get('/');
-    // });
-
-    // var params = browser.params;
-
-    // browser.driver.manage().window().maximize();
-
-    // it('should be able to reset his password', function(done) {
-        // var login = element(by.css('.b-header-nav li#login'));
-        // login.click();
-
-        // var forgotPw = element.all(by.css('.forgot-password')).first();
-        // forgotPw.element(by.css('span')).click();
-
-        // element(by.model('emailForgot')).sendKeys(params.signup.email);
-        // element(by.css('form[name="forgotForm"] button[type="submit"]')).click();
-
-        // browser.sleep(1000);
-
-// //        browser.get('http://shitmail.me/mail/inbox/somuchmore@shitmail.me');
-// //        var btn = element(by.cssContainingText('a', 'Passwort reset request'));
-// //        btn.click();
-// //
-// //        browser.driver.executeScript(function() {
-// //            return document.querySelectorAll('.boxContent')[1].innerText.match('https.*')[0];
-// //        }).then(function(result) {
-// //            browser.get(result);
-// //        });
-// //
-// //        var resetPage = element(by.css('.b-main-slogan'));
-// //        expect(browser.getCurrentUrl()).toContain('/p/password');
-// //
-// //        element(by.css('form.b-card-subscribe input[type="password"]')).sendKeys(params.signup.password);
-// //        element(by.css('form.b-card-subscribe button[type="submit"]')).click();
-
-        // done();
-    // });
-// });
