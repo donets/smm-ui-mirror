4ba307649387f4d65afc4bb0a6d490dda4f6f1b5
donets <mmf.donec@gmail.com>
2015-04-09 23:19:39 +0200

    SMMWEB-212

--- app/scripts/controllers/studio.js
+++ app/scripts/controllers/studio.js
@@ -8,17 +8,17 @@
  * Controller of the boltApp
  */
 angular.module('boltApp.controllers.Studio', ['uiGmapgoogle-maps'])
-    .controller('StudioCtrl', function ($scope, $rootScope, $q, $modal, getStudio, RestApi, uiGmapGoogleMapApi) {
+    .controller('StudioCtrl', function ($scope, $rootScope, $q, $modal, getStudio, $http, $window, RestApi, uiGmapGoogleMapApi) {
         $scope.coverMain = $rootScope.windowWidth > 1080 ? '/images/landing-2880.jpg' : '/images/landing-1080.jpg';
         $scope._ = _;
         getStudio.$promise.then(function () {
             $scope.studio = getStudio;
             $scope.studio.locationsFull = [];
-            $q.all([RestApi.query({route: 'locations'}).$promise, RestApi.query({route: 'events'}).$promise]).then(function (res) {
+            RestApi.query({route: 'locations'}).$promise.then(function (res) {
                 _.each($scope.studio.locations, function (locationId) {
-                    $scope.studio.locationsFull.push(_.findWhere(res[0], {id: locationId}));
+                    $scope.studio.locationsFull.push(_.findWhere(res, {id: locationId}));
                 });
-                $scope.classes = res[1];
+                $scope.currDay = moment();
                 $scope.setLocation($scope.studio.locationsFull[0]);
             });
         });
@@ -29,20 +29,24 @@ angular.module('boltApp.controllers.Studio', ['uiGmapgoogle-maps'])
         $scope.today = moment();
         $scope.changeDay = function (day) {
             $scope.currDay = day;
+            $scope.setLocation($scope.currLocation);
         };
-        $scope.changeDay($scope.today);
         for (var d = 0; d < 7; d++) {
             $scope.weekdays.push(moment().add(d, 'day'));
         }
 
         $scope.setLocation = function (location) {
             $scope.currLocation = location;
-            RestApi.query({route: 'occurrences', forDurationOfDays: 7, withActiveParent: true, locationId: location.id}).$promise.then(function (response) {
-                $scope.events = _.each(response, function (event) {
-                    event.start_date = moment(event.start_date);
-                    event.end_date = moment(event.end_date);
-                    event.class = _.findWhere($scope.classes, {id: event.parent_event_id});
+            $scope.showSpinner = true;
+            $http.post($window.smmConfig.restUrlBase + '/api/classes/get/all', {locationId: location.id, date: $scope.currDay.format('YYYY-MM-DD')}).success(function (res) {
+                $scope.events = _.each(res.classes.occurenceAccesses, function (event) {
+                    event.start_date = moment(event.date + 'T' + event.startTime);
+                    event.end_date = moment(event.date + 'T' + event.endTime);
+                    event.startTime = event.startTime.slice(0,5);
+                    event.endTime = event.endTime.slice(0,5);
+                    event.class = _.findWhere(res.classes.classAccesses, {id: event.classId});
                 });
+                $scope.showSpinner = false;
             });
             uiGmapGoogleMapApi.then(function() {
 
