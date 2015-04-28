eb00077801a2eabe0d6639fd7aaf471016d8eae1
macabre133 <xenixlx@gmail.com>
2015-04-28 10:50:25 +0300

    commented protractor in Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -430,15 +430,15 @@ module.exports = function(grunt) {
             }
         },
 
-        protractor: {
-            options: {
-                configFile: 'test/protractor-conf.js',
-                keepAlive: false, // If false, the grunt process stops when the test fails.
-                noColor: false, // If true, protractor will not use colors in its output.
-                args: {}
-            },
-            all: {}
-        },
+        // protractor: {
+            // options: {
+                // configFile: 'test/protractor-conf.js',
+                // keepAlive: false, // If false, the grunt process stops when the test fails.
+                // noColor: false, // If true, protractor will not use colors in its output.
+                // args: {}
+            // },
+            // all: {}
+        // },
 
         nggettext_extract: {
             pot: {
