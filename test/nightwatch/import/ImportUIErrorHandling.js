0407005f6880eeaa3941b0fb7d0ffdf7daae1a2d
macabre133 <xenixlx@gmail.com>
2015-05-05 19:27:12 +0300

    fixes

--- test/nightwatch/import/ImportUIErrorHandling.js
+++ test/nightwatch/import/ImportUIErrorHandling.js
@@ -5,14 +5,14 @@ module.exports = {
     var params = browser.globals;
     browser
       .url(params.baseUrl + '/?city=L')
-      .waitForElementVisible('body', browser.globals.waitUI)
+      .waitForElementVisible('.login-link > span:nth-child(1)', browser.globals.waitUI)
 	  .assert.elementPresent('.login-link > span:nth-child(1)')
       .click('.login-link > span:nth-child(1)')
 	  .assert.elementPresent('input[name=email]')
 	  .assert.elementPresent('input[name=password]')
       .waitForElementVisible('input[name=email]', browser.globals.waitUI)
-      .setValue('input[name=email]', params.signup.email)
-      .setValue('input[name=password]', params.signup.password)
+      .setValue('input[name=email]', params.admin.email)
+      .setValue('input[name=password]', params.admin.password)
       .click('button[type=submit]')
 	  .waitForElementVisible('#account', browser.globals.waitPOST)
       .assert.elementPresent('#account');
@@ -21,19 +21,18 @@ module.exports = {
   'Go to my account and upload photo': function (browser) {
      var path = require('path');
      var params = browser.globals;
-      fileToUpload = '../../app/images/alexandra.jpg',
+      fileToUpload = '../../test/test_import.csv',
       absolutePath = path.resolve(__dirname, fileToUpload);
+	  console.log(absolutePath);
     browser
-      .url(params.baseUrl + '/my/account/')
-      .waitForElementVisible('.profile-title > h2:nth-child(1) > span:nth-child(1)', browser.globals.waitPOST)
-      .assert.containsText('.profile-title > h2:nth-child(1) > span:nth-child(1)', 'Hello')
-      .click('.tab-nav > li:nth-child(1) > a:nth-child(1)')
-      .waitForElementVisible('div.env-upload-box > div',browser.globals.waitUI)
-      .click('div.env-upload-box > div')
-      .execute('$(\'.button_upload input\').css("visibility", "visible");')
-      .setValue('.button_upload input',absolutePath)
-      .click('.action .upload_button')
-      .waitForElementNotVisible('div.modal-dialog',browser.globals.waitPOST)
+      .url(params.baseUrl + '/admin/v2/classes/import/')
+      .waitForElementVisible('.btn', browser.globals.waitPOST)
+	  .execute('$(\'.btn\').click(function(e){e.preventDefault();});')
+	  //.execute('$(\'.btn\').click()')
+      .setValue('.btn',absolutePath)
+	  //.fireEvent('change')
+	  .execute('$(\'.btn\').change()')
+      //.waitForElementNotVisible('div.modal-dialog',browser.globals.waitPOST)
       .end();//add assertion of image for complete test
   }
 };
