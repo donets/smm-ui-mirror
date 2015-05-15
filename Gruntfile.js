d10f03649e226500baf2faa6ca7dccaa3ac0ce02
donets <mmf.donec@gmail.com>
2015-05-15 22:58:36 +0300

    add ng templates task to gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -560,7 +560,7 @@ module.exports = function(grunt) {
 
     grunt.registerTask('build', [
         'clean:dist',
-        //'wiredep',
+        'ngtemplates',
         'useminPrepare',
         'concurrent:dist',
         'autoprefixer',
