5da78786eed606a0b3662f0163b030206af67901
Florian Biewald <f.biewald@googlemail.com>
2015-05-22 08:41:49 +0200

    grunt serve will spawn nodemon now

--- Gruntfile.js
+++ Gruntfile.js
@@ -556,6 +556,16 @@ module.exports = function(grunt) {
             return grunt.task.run(['build', 'connect:dist:keepalive']);
         }
 
+        // Running nodejs in a different process and displaying output on the main console
+       var nodemon = grunt.util.spawn({
+           cmd: 'grunt',
+           grunt: true,
+           args: 'nodemon'
+       });
+
+       nodemon.stdout.pipe(process.stdout);
+       nodemon.stderr.pipe(process.stderr);
+
         grunt.task.run([
             'clean:server',
             'concurrent:server',
