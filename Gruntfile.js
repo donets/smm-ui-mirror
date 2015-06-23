272a5863b48172d6f4724d13ac60c9d58d019f98
donets <mmf.donec@gmail.com>
2015-06-23 15:03:45 +0300

    restrict update index from bower

--- Gruntfile.js
+++ Gruntfile.js
@@ -34,10 +34,6 @@ module.exports = function(grunt) {
 
         // Watches files for changes and runs tasks based on the changed files
         watch: {
-            bower: {
-                files: ['bower.json'],
-                tasks: ['wiredep']
-            },
             js: {
                 files: ['<%= yeoman.app %>/scripts/{,*/}*.js'],
                 tasks: ['newer:jshint:all'],
